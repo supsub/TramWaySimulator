216bf56b0482a7d328c38a241d58a296025a7e95
SAMSUNG <juliusz.rylko@gmail.com>
2018-12-16 19:32:40 +0100

    added timer, speed up button, speed down button

--- src/app.py
+++ src/app.py
@@ -1,53 +1,65 @@
-from tkinter import Tk, Label, Frame,SUNKEN,Scrollbar,HORIZONTAL,Canvas,N,S,E,W,BOTH,ALL,simpledialog
+from tkinter import Tk, Label, Button,Frame,SUNKEN,Scrollbar,HORIZONTAL,Canvas,N,S,E,W,BOTH,ALL,simpledialog
 from PIL import Image, ImageTk
 import json
 import numpy as np
+import time
 
 
 STOP_COLOR = 'red'
 STOP_RADIUS = 7
 TRAM_COLOR = 'yellow'
-TRAM_RADIUS = 7
+TRAM_RADIUS = 9
 LIGHT_RADIUS = 4
+
+START_TIME = 6*3600
 
 with open('data/data.json') as f:
     STOP_DATA = json.load(f)
 
 class Tram:
-    def __init__(self,canvas,route,times,intersections):
+    def __init__(self,canvas,route,times):
+        self.time_factor = 1
         self.stop_count = 0
-        self.intersections_passed = 0
         self.counter = 0
         self.time_to_departure = 0
         self.route = route
         self.times = times
         self.canvas = canvas
         self.time = self.times[self.stop_count]
-        x, y = route[0]
         self.in_action =True
         self.moving = True
         self.calculate_direction()
-        self.draw(x, y)
+        self.draw()
 
-    def draw(self, x, y):
+
+    def draw(self):
+        x, y = self.route[0]
         self.body = self.canvas.create_oval(int(x), int(y), int(x + 2 * TRAM_RADIUS), int(y + 2 * TRAM_RADIUS),
                                             fill=TRAM_COLOR)
 
-    def calculate_direction(self):
-        self.direction = np.subtract(self.route[self.stop_count + 1], self.route[self.stop_count])
+        self.text = self.canvas.create_text(int(x+TRAM_RADIUS), int(y+TRAM_RADIUS),fill='black',text='50',font="Times 7 italic bold",
+                                            activefill='red')
 
     def move(self):
-        if self.time_to_departure>0:
-            self.time_to_departure -=1
+        if self.time_to_departure > 0:
+            self.time_to_departure -= 1*self.time_factor
+        else:
+            self.moving =True
         if self.in_action and (self.moving or self.time_to_departure == 0):
             self.action()
 
     def action(self):
-        x, y = np.divide(self.direction, self.time)
+        x, y = np.multiply(np.divide(self.direction, self.time),self.time_factor)
         self.canvas.move(self.body, x, y)
-        self.counter += 1
-        if self.counter == self.time:
+        self.canvas.move(self.text, x, y)
+        self.counter += 1*self.time_factor
+        if self.counter >= self.time-self.time_factor:
+            self.canvas.coords(self.body,self.get_4_coords_of_stop(self.route[self.stop_count + 1]))
+            self.canvas.coords(self.text, tuple(np.add(self.route[self.stop_count + 1],(TRAM_RADIUS,TRAM_RADIUS))))
             self.change_direction()
+    def get_4_coords_of_stop(self,coords):
+        return (coords[0],coords[1],coords[0]+2*TRAM_RADIUS,coords[1]+2*TRAM_RADIUS)
+
 
     def change_direction(self):
         self.counter = 0
@@ -60,41 +72,47 @@ class Tram:
         else:
             self.in_action = False
 
+    def calculate_direction(self):
+        self.direction = np.subtract(self.route[self.stop_count + 1], self.route[self.stop_count])
 
-class TrafficLight:
-    def __init__(self, canvas,position):
-        self.light_timer = 30
-        self.canvas = canvas
-        self.x, self.y = position
-        self.preference = 'A'
-        self.change_color()
-        self.body = self.canvas.create_oval(int(self.x), int(self.y), int(self.x + 2*LIGHT_RADIUS),int(self.y + 2*LIGHT_RADIUS),fill=self.color)
-        self.text = self.canvas.create_text(self.x-20, self.y, fill=self.color, font="Times 11 italic bold",
-                                text=self.preference)
 
-    def change_color(self):
-        if self.preference == 'A':
-            self.color = 'green'
-        else:
-            self.color = 'red'
-
-    def change(self):
-        if self.preference == 'A':
-            self.preference = 'B'
-        else:
-            self.preference = 'A'
-        self.change_color()
-        self.change_items_color()
-
-    def change_items_color(self):
-        self.canvas.itemconfig(self.body, fill=self.color)
-        self.canvas.itemconfig(self.text, fill=self.color, text=self.preference)
-
-    def animate(self):
-        self.light_timer -=1
-        if self.light_timer ==0:
-            self.light_timer = 30
-            self.change()
+# class Intersection:
+#     def __init__(self, canvas,position):
+#         self.light_timer = 30
+#         self.canvas = canvas
+#         self.position = position
+#         self.x, self.y = self.position
+#         self.preference = 'A'
+#         self.change_color()
+#         self.body = self.canvas.create_oval(int(self.x), int(self.y), int(self.x + 2*LIGHT_RADIUS),int(self.y + 2*LIGHT_RADIUS),fill=self.color)
+#         self.text = self.canvas.create_text(self.x-20, self.y, fill=self.color, font="Times 11 italic bold",
+#                                 text=self.preference)
+#
+#
+#
+#     def change_color(self):
+#         if self.preference == 'A':
+#             self.color = 'green'
+#         else:
+#             self.color = 'red'
+#
+#     def change(self):
+#         if self.preference == 'A':
+#             self.preference = 'B'
+#         else:
+#             self.preference = 'A'
+#         self.change_color()
+#         self.change_items_color()
+#
+#     def change_items_color(self):
+#         self.canvas.itemconfig(self.body, fill=self.color)
+#         self.canvas.itemconfig(self.text, fill=self.color, text=self.preference)
+#
+#     def animate(self):
+#         self.light_timer -=1
+#         if self.light_timer ==0:
+#             self.light_timer = 30
+#             self.change()
 class MyFirstGUI:
     def __init__(self, master,filename,stops_json,traffic_lights_json):
         with open(stops_json) as f:
@@ -105,11 +123,9 @@ class MyFirstGUI:
 
         self.master = master
         self.filename = filename
+        self.trams = []
         self.setup()
         self.create_stops()
-        self.lights = []
-        #self.create_lights()
-        self.lights.append(TrafficLight(self.canvas,get_coords_of_stop("Rondo Mogilskie")))
 
 
         route1 = [
@@ -141,21 +157,22 @@ class MyFirstGUI:
         coords1 = get_route_coords(route1)
 
         self.draw_route(coords1)
-        self.trams = [Tram(self.canvas,coords1,np.int_(times1),{'Rondo Mogilskie':'A'}),Tram(self.canvas,coords1,np.int_(times2),{'Rondo Mogilskie':'A'})]
+        self.trams = [Tram(self.canvas,coords1,np.int_(times1)),Tram(self.canvas,coords1,np.int_(times2))]
         self.master.after(0,self.animation)
 
-    def create_lights(self):
-
-        for light in self.traffic_lights_data:
-            type = light['stop_name']
-            x = light['x_coord']
-            y= light['y_coord']
-            self.lights.append(TrafficLight(self.canvas,(x,y),type))
 
     def setup(self):
         self.master.title("Symulacja krakowskich lini tramwajowych")
         self.label = Label(self.master, text="Mapa")
         self.label.pack()
+        self.time_factor = 1
+        self.seconds = START_TIME
+        self.time = Label(self.master, text="Godzina: {}".format(time.strftime('%H:%M:%S', time.gmtime(self.seconds))))
+        self.time.pack()
+        self.button = Button(self.master,text="speedUp",command = self.speed_up)
+        self.button.pack()
+        self.button = Button(self.master, text="speedDown", command=self.speed_down)
+        self.button.pack()
         self.frame = Frame(root, bd=2, relief=SUNKEN)
         self.frame.grid_rowconfigure(0, weight=1)
         self.frame.grid_columnconfigure(0, weight=1)
@@ -178,9 +195,12 @@ class MyFirstGUI:
         self.canvas.create_image(0, 0, image=self.img, anchor="nw")
         self.canvas.config(scrollregion=self.canvas.bbox(ALL))
 
+
+
     def draw_route(self,coords):
         for i in range(len(coords)-1):
-            self.canvas.create_line(coords[i][0],coords[i][1],coords[i+1][0],coords[i+1][1],width=3)
+            self.canvas.create_line(coords[i][0]+TRAM_RADIUS,coords[i][1]+TRAM_RADIUS,
+                                    coords[i+1][0]+TRAM_RADIUS,coords[i+1][1]+TRAM_RADIUS,width=3)
 
     def _on_mousewheel(self, event):
         self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
@@ -193,7 +213,6 @@ class MyFirstGUI:
         self.canvas.create_text(x, y-20, fill="darkblue", font="Times 11 italic bold",
                                 text=name)
     def create_stops(self):
-
         for stop in self.stops_data:
             stop_name = stop['stop_name']
             x_coord = stop['x_coord']
@@ -203,10 +222,18 @@ class MyFirstGUI:
     def animation(self):
         for tram in self.trams:
             tram.move()
-        for light in self.lights:
-            light.animate()
+        self.seconds += 0.042*self.time_factor
+        self.time.config(text="Godzina: {}".format(time.strftime('%H:%M:%S', time.gmtime(self.seconds))))
         self.master.after(42,self.animation)
 
+    def speed_up(self):
+        self.time_factor *= 2
+        for tram in self.trams:
+            tram.time_factor =self.time_factor
+    def speed_down(self):
+        self.time_factor /= 2
+        for tram in self.trams:
+            tram.time_factor = self.time_factor
 
 
 ### tu funkcja która ci zamienia trase typu ['Wesele', 'Bronowice', 'Głowackiego']
